60f4745602ef0a3273eb7e492c9c313e75f367d8
IAM5KID <01fe19bee418@gmail.com>
2023-07-03 00:26:52 +0530

    lol

--- src/index.js
+++ src/index.js
@@ -4,18 +4,11 @@ import './index.css';
 import {BrowserRouter} from "react-router-dom";
 import App from "./App"
 
-// ReactDOM.render(
-//   <>
-//     <BrowserRouter basename={process.env.PUBLIC_URL}>
-//       <App/>
-//     </BrowserRouter>
-//   </>,
-//   document.getElementById("root")
-// );
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <>
+    <BrowserRouter basename="/myfirstReactWebsite/">
+      <App/>
+    </BrowserRouter>
+  </>,
+  document.getElementById("root")
 );
