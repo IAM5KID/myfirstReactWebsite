59e30c2e199bedf889c8d1a179db79a803e5ed5a
IAM5KID <01fe19bee418@gmail.com>
2023-07-03 00:21:01 +0530

    now?

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import {BrowserRouter} from "react-router-dom";
 import App from "./App"
 
-ReactDOM.render(
-  <>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App/>
-    </BrowserRouter>
-  </>,
-  document.getElementById("root")
+// ReactDOM.render(
+//   <>
+//     <BrowserRouter basename={process.env.PUBLIC_URL}>
+//       <App/>
+//     </BrowserRouter>
+//   </>,
+//   document.getElementById("root")
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+reportWebVitals();
